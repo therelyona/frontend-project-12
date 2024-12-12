411405fc77439244382b948dd97d05ca944ff128
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-12 20:35:32 +0400

    added LoginPage, NotFoundPage in frontend/src/pages/

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import Container from '../components/Container';
+import Image from '../components/Image';
+import loginImage from '../assets/loginImage.png';
+import LoginForm from '../components/LoginForm';
+import routes from '../utils/routes';
+
+const LoginPage = () => (
+  <Container>
+    <div className="card-body row p-5">
+      <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+        <Image src={loginImage} alt="Войти" />
+      </div>
+      <LoginForm />
+    </div>
+    <div className="card-footer p-4">
+      <div className="text-center">
+        <span>Нет аккаунта?</span>
+        <Link to={routes.signUpPagePath()}>Регистрация</Link>
+      </div>
+    </div>
+  </Container>
+);
+
+export default LoginPage;
---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import notFoundImage from '../assets/notFoundImage.png';
+import routes from '../utils/routes.js';
+import Image from '../components/Image.jsx';
+
+const NotFoundPage = () => (
+  <div className="text-center">
+    <Image
+      src={notFoundImage}
+      alt="Страница не найдена"
+    />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      <Link to={routes.homePagePath()}>на главную страницу</Link>
+    </p>
+  </div>
+);
+
+export default NotFoundPage;
