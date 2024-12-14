bbbc35571c394126c35d38baa411f15ad2f04ffe
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-14 21:12:40 +0400

    add new routes and rename homePagePath to mainPagePath

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -1,7 +1,11 @@
+const apiPath = '/api/v1';
+
 const routes = {
-  homePagePath: () => '/',
+  mainPagePath: () => '/',
   loginPagePath: () => '/login',
   signUpPagePath: () => '/signup',
+  loginApiPath: () => [apiPath, 'login'].join('/'),
+  signUpApiPath: () => [apiPath, 'signup'].join('/'),
 };
 
 export default routes;
