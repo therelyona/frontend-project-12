2354edc79f64d9966f371ad9eb266581ca78e515
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-14 20:58:58 +0400

    replace custom Navbar component with React-Bootstrap Navbar

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,8 +1,13 @@
-import Navbar from './Navbar.jsx';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const MainLayout = ({ children }) => (
   <div className="d-flex flex-column h-100">
-    <Navbar />
+    <Navbar className="shadow-sm bg-white" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+      </Container>
+    </Navbar>
     {children}
   </div>
 );
