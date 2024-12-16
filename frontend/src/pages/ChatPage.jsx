4fe04c228bc6a22f2849b045f829b6855d61d2d4
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-17 00:13:44 +0400

    add ChatPage

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import routes from '../utils/routes';
+import ChatContainer from '../components/Chat/ChatContainer';
+
+const ChatPage = () => {
+  const { loggedIn } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate(routes.loginApiPath());
+    }
+  }, []);
+
+  return (
+    <ChatContainer />
+  );
+};
+
+export default ChatPage;
