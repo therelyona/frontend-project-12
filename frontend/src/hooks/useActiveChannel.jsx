8d75ccac563b942b1e9db0a501eb0fe50b053154
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-17 00:13:20 +0400

    created useActiveChannel hook for managing active channel state

---
+++ frontend/src/hooks/useActiveChannel.jsx
@@ -0,0 +1,18 @@
+import { useGetChannelsQuery } from '../store/api/chatApi';
+import defaultChannel from '../store/slices/defaultChannel';
+
+const useActiveChannel = (channel) => {
+  const { data: channels, isLoading } = useGetChannelsQuery();
+  if (isLoading) {
+    return { activeChannelId: null, activeChannelName: 'Loading...' };
+  }
+  const foundChannel = channels?.find((c) => c.id === channel.id);
+  const activeChannel = foundChannel || defaultChannel;
+  const activeChannelId = activeChannel.id;
+  const activeChannelName = activeChannel.name;
+  return {
+    activeChannelId, activeChannelName, channels, isLoading,
+  };
+};
+
+export default useActiveChannel;
