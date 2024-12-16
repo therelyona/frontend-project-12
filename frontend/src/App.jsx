471aca761bc31772a4abbbffaa689dc010ea1fe7
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-16 23:52:13 +0400

    added Provider and Route for mainPagePath

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import routes from './utils/routes';
 import NotFoundPage from './pages/NotFoundPage';
-import MainLayout from './components/MainLayout';
+import MainLayout from './components/Pages/MainLayout';
 import LoginPage from './pages/LoginPage';
 import AuthProvider from './contexts/AuthProvider';
+import store from './store/store';
+import ChatPage from './pages/ChatPage';
 
 const App = () => (
   <BrowserRouter>
-    <AuthProvider>
-      <MainLayout>
-        <Routes>
-          <Route path={routes.mainPagePath()} element={<LoginPage />} />
-          <Route path={routes.loginPagePath()} element={<LoginPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </MainLayout>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <MainLayout>
+          <Routes>
+            <Route path={routes.mainPagePath()} element={<ChatPage />} />
+            <Route path={routes.loginPagePath()} element={<LoginPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </MainLayout>
+      </AuthProvider>
+    </Provider>
   </BrowserRouter>
 );
 
