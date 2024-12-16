1bc0f9c44669257b1f8e52657dec0f95c12cf5c3
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-17 00:08:17 +0400

    added ChannelTitle component

---
+++ frontend/src/components/Chat/ChannelTitle.jsx
@@ -0,0 +1,10 @@
+import ChannelButtonAdd from './ChannelButtonAdd';
+
+const ChannelTitle = () => (
+  <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+    <b>Каналы</b>
+    <ChannelButtonAdd />
+  </div>
+);
+
+export default ChannelTitle;
