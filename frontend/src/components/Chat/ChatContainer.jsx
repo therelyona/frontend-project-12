31b6ee3fbd91cadab5b0db3c21c0cae4672f2b26
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-17 19:25:07 +0400

    added MessagesContainer component to ChatContainer

--- frontend/src/components/Chat/ChatContainer.jsx
+++ frontend/src/components/Chat/ChatContainer.jsx
@@ -1,9 +1,11 @@
 import ChannelList from './ChannelList';
+import MessagesContainer from './MessagesContainer';
 
 const ChatContainer = () => (
   <div className="container h-100 my-4 overflow-hidden rounded shadow">
     <div className="row h-100 bg-white flex-md-row">
       <ChannelList />
+      <MessagesContainer />
     </div>
   </div>
 );
