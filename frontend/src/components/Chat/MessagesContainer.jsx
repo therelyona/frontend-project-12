31aa85c9b5dd75b43afbcb1fe7ca367cb1eb0efd
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-17 19:23:53 +0400

    added MessagesContainer.jsx

---
+++ frontend/src/components/Chat/MessagesContainer.jsx
@@ -0,0 +1,43 @@
+import { useSelector } from 'react-redux';
+import { activeChannelSelector } from '../../store/slices/activeChannelSlice';
+import useActiveChannel from '../../hooks/useActiveChannel';
+import {
+  useAddMessagesMutation,
+  useGetMessagesQuery,
+} from '../../store/api/chatApi';
+import MessagesBox from './MessagesBox';
+import MessagesForm from './MessagesForm';
+
+const MessagesContainer = () => {
+  const activeChannel = useSelector(activeChannelSelector);
+  const { activeChannelId, activeChannelName } = useActiveChannel(activeChannel);
+  const { data: messages, isLoading } = useGetMessagesQuery;
+  const [addMessage] = useAddMessagesMutation();
+  const channelMessages = messages?.filter((message) => message.channelId === activeChannelId);
+  const countMessages = channelMessages?.length || 0;
+  const username = localStorage.getItem('username');
+
+  return (
+    <div className="col p-0 h-100">
+      <div className="d-flex flex-column h-100">
+        <div className="bg-light mb-4 p-3 shadow-sm small">
+          <p className="m-0">
+            <b>{`# ${activeChannelName}`}</b>
+          </p>
+          <span className="text-muted">{`${countMessages} сообщений`}</span>
+        </div>
+        <MessagesBox channelMessages={channelMessages} />
+        <div className="mt-auto px-5 py-3">
+          <MessagesForm
+            channelId={activeChannelId}
+            username={username}
+            addMessage={addMessage}
+            isLoading={isLoading}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MessagesContainer;
