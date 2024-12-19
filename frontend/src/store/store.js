058ca3f63a698689e181fd73aa4fb1b81ac3d5fb
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-19 19:53:46 +0400

    add WebSocket listeners and setupListeners for chatApi in store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import activeChannelReducer from './slices/activeChannelSlice';
 import { chatApi } from './api/chatApi';
+import setupWebSocketListeners from './api/webSocketHandler';
 
 const store = configureStore({
   reducer: {
@@ -9,5 +11,8 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(chatApi.middleware),
 });
+
+setupWebSocketListeners(store);
+setupListeners(store.dispatch);
 
 export default store;
