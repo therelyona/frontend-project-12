22e0a628e7797dc0d2d70176d2fdee21601b41ad
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-23 17:57:11 +0400

    added reducer for modal management

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import activeChannelReducer from './slices/activeChannelSlice';
+import modalReducer from './slices/modalSlice';
 import { chatApi } from './api/chatApi';
 import setupWebSocketListeners from './api/webSocketHandler';
 
@@ -8,6 +9,7 @@ const store = configureStore({
   reducer: {
     [chatApi.reducerPath]: chatApi.reducer,
     activeChannel: activeChannelReducer,
+    modal: modalReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(chatApi.middleware),
 });
