dfa30663106ef4c0fe0a8c64938d82c99e1c5435
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-23 17:31:00 +0400

    added onClick handler to open add modal window

--- frontend/src/components/Chat/ChannelButtonAdd.jsx
+++ frontend/src/components/Chat/ChannelButtonAdd.jsx
@@ -1,5 +1,5 @@
-const ChannelButtonAdd = () => (
-  <button type="button" className="p-0 text-primary btn btn-group-vertical">
+const ChannelButtonAdd = ({ openModal }) => (
+  <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={() => openModal('adding')}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 16 16"
