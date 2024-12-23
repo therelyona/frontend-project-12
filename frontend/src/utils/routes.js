35902ebdc75996e777fcd525be8acc353a435ebc
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-23 22:03:49 +0400

    removed API paths for login and signup from the routes object

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -1,11 +1,7 @@
-const apiPath = '/api/v1';
-
 const routes = {
   mainPagePath: () => '/',
   loginPagePath: () => '/login',
   signUpPagePath: () => '/signup',
-  loginApiPath: () => [apiPath, 'login'].join('/'),
-  signUpApiPath: () => [apiPath, 'signup'].join('/'),
 };
 
 export default routes;
