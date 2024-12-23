8d06c36482945949bb4ad158a7fbc652a3882708
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-23 17:56:16 +0400

    added handlers for adding, removing, and renaming channels via websockets

--- frontend/src/store/api/webSocketHandler.js
+++ frontend/src/store/api/webSocketHandler.js
@@ -10,6 +10,30 @@ const setupWebSocketListeners = (store) => {
       draft.push(newMessage);
     }));
   });
+
+  socket.on('newChannel', (newChannel) => {
+    dispatch(chatApi.util.updateQueryData('getChannels', undefined, (draft) => {
+      draft.push(newChannel);
+    }));
+  });
+
+  socket.on('removeChannel', (channel) => {
+    dispatch(chatApi.util.updateQueryData('getChannels', undefined, (draft) => {
+      const index = draft.findIndex((c) => c.id === channel.id);
+      if (index !== -1) {
+        draft.splice(index, 1);
+      }
+    }));
+  });
+
+  socket.on('renameChannel', (channel) => {
+    dispatch(chatApi.util.updateQueryData('getChannels', undefined, (draft) => {
+      const currentChannel = draft.find((c) => c.id === channel.id);
+      if (currentChannel) {
+        currentChannel.name = channel.name;
+      }
+    }));
+  });
 };
 
 export default setupWebSocketListeners;
