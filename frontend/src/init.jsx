20831ea7978bcd6ac9b7a533f421676c9870cc0a
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-24 20:55:35 +0400

    added i18next initialization for localization

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,7 +1,21 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import resources from './locales/index';
 import App from './App';
 
-const init = () => (
-  <App />
-);
+const init = () => {
+  const i18n = i18next.createInstance();
+  const options = {
+    resources,
+    fallbackLng: 'ru',
+    interpolation: {
+      escapeValue: false,
+    },
+  };
+
+  i18n.use(initReactI18next).init(options);
+
+  return <App />;
+};
 
 export default init;
