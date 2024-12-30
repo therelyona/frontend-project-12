8c39af68a11812070d32c4a5c0dc14e2262dfaf7
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-30 20:12:10 +0400

    added useActiveChannel hook

--- frontend/src/components/Chat/DropdownButton.jsx
+++ frontend/src/components/Chat/DropdownButton.jsx
@@ -1,17 +1,19 @@
 import { Dropdown, ButtonGroup } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { activeChannelSelector } from '../../store/slices/activeChannelSlice';
+import useActiveChannel from '../../hooks/useActiveChannel';
 import ChannelItem from './ChannelItem';
 
 const DropdownButton = ({ openModal, channel, t }) => {
   const activeChannel = useSelector(activeChannelSelector);
+  const { activeChannelId } = useActiveChannel(activeChannel);
 
   return (
     <Dropdown as={ButtonGroup} className="me-2 w-100">
       <ChannelItem channel={channel} />
       <Dropdown.Toggle
         split
-        variant={activeChannel.id === channel.id ? 'secondary' : ''}
+        variant={activeChannelId === channel.id ? 'secondary' : ''}
         id="channelDropdown"
       >
         <span className="visually-hidden">{t('dropdownButton.description')}</span>
