9341d18235b3e3ae4c629b3a5a90efa068325b73
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-12-30 20:13:07 +0400

    added useRef hook for auto-scrolling messages to the bottom

--- frontend/src/components/Chat/MessagesBox.jsx
+++ frontend/src/components/Chat/MessagesBox.jsx
@@ -1,16 +1,26 @@
-const MessagesBox = ({ channelMessages }) => (
-  <div
-    id="messages-box"
-    className="chat-messages overflow-auto px-5"
-  >
-    {channelMessages?.map(({ id, username, body }) => (
-      <div className="ext-break mb-2" key={id}>
-        <b>{username}</b>
-        {': '}
-        <span>{body}</span>
-      </div>
-    ))}
-  </div>
-);
+import { useEffect, useRef } from 'react';
+
+const MessagesBox = ({ channelMessages }) => {
+  const messagesEndRef = useRef(null);
+  useEffect(() => {
+    messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+  }, [channelMessages]);
+
+  return (
+    <div
+      id="messages-box"
+      className="chat-messages overflow-auto px-5"
+      ref={messagesEndRef}
+    >
+      {channelMessages?.map(({ id, username, body }) => (
+        <div className="ext-break mb-2" key={id}>
+          <b>{username}</b>
+          {': '}
+          <span>{body}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default MessagesBox;
