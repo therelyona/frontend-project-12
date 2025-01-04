9c34a64f308dd992ff386fbfadbc4d7f8e800afe
soboleva.alyona <soboleva.alyonka@gmail.com>
2025-01-04 21:07:28 +0400

    used await with init in root render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import init from './init';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const vdom = init();
+const vdom = await init();
 root.render(
   vdom,
 );
