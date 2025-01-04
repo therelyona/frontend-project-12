add82514e7512177f04d013da8806e27881941d6
soboleva.alyona <soboleva.alyonka@gmail.com>
2025-01-04 21:06:24 +0400

    added formik.isSubmitting check to button disable condition

--- frontend/src/components/Chat/MessagesForm.jsx
+++ frontend/src/components/Chat/MessagesForm.jsx
@@ -1,13 +1,15 @@
 import { Form } from 'react-bootstrap';
 import { useEffect, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useFormik } from 'formik';
 import { toast } from 'react-toastify';
 import filter from 'leo-profanity';
 import MessageButtonInput from './MessageButtonInput';
 
 const MessagesForm = ({
-  channelId, addMessage, username, isLoading, t,
+  channelId, addMessage, username, isLoading,
 }) => {
+  const { t } = useTranslation();
   const inputRef = useRef(null);
 
   useEffect(() => {
@@ -58,7 +60,7 @@ const MessagesForm = ({
         <button
           type="submit"
           className="btn btn-group-vertical"
-          disabled={!formik.values.body.trim() || isLoading}
+          disabled={!formik.values.body.trim() || isLoading || formik.isSubmitting}
         >
           <MessageButtonInput />
           <span className="visually-hidden">{t('messagasForm.submitButton')}</span>
