fe7ee4ce390c98d5f748192c059edb76a2e2fedf
soboleva.alyona <soboleva.alyonka@gmail.com>
2025-01-04 21:10:39 +0400

    removed auth check

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,22 +1,7 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import useAuth from '../hooks/useAuth';
-import routes from '../utils/routes';
 import ChatContainer from '../components/Chat/ChatContainer';
 
-const ChatPage = () => {
-  const { loggedIn } = useAuth();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!loggedIn) {
-      navigate(routes.loginPagePath());
-    }
-  }, []);
-
-  return (
-    <ChatContainer />
-  );
-};
+const ChatPage = () => (
+  <ChatContainer />
+);
 
 export default ChatPage;
